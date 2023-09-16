80547a13e6ed1876b76252786c34d71cb4ca527f
joabealves <joabe66@gmail.com>
2023-09-15 21:23:59 -0300

    comitar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,152 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill, BsFillPieChartFill } from "react-icons/bs";
+
+const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [fim, setfim] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //Load todos on page Load
+  useEffect(() => {
+
+    const loadData = async () => {
+      setLoading(true)
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err));
+
+      setLoading(false);
+      setTodos(res);
+
+    };
+    loadData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log("Enviou!");   
+
+    
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      fim,     
+      done: false,
+
+    };
+ 
+    
+    if (todo.fim > todo.time) {
+      await fetch(API + "/todos/", {
+        method: "POST",
+        body: JSON.stringify(todo),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setTodos((prevState) => [...prevState, todo]);
+      setTitle(" ");
+      setTime(" ");
+      setfim(" ");
+
+
+    } else {
+      return alert (`o valor final ${fim} hrs, não pode ser menor que o valor inicial ${time} hrs : `);
+    };
+
+
+
+  };
+
+
+
+  const handleDelete = async (id) => {
+
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+
+  };
+
+  const handleEdit = async (todo) => {
+    todo.done = !todo.done;
+    const data = await BsFillPieChartFill(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) => prevState.map((T) => (T.id === data.id ? (T = data) : T)));
+
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-header">
+        <h1>Gerencie suas tarefas </h1>
+      </div>
+
+      <div className="form-todo">
+        <h2>Insira sua próxima tarefa</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">Qual tarefa deseja fazer ?</label>
+            <input type="text" name="title" placeholder="Título da tarefa"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""} required />
+          </div>
+
+          <div className="form-control ">
+            <label htmlFor="time">Inicio da tarefa:</label>
+            <input type="time" name="time"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""} required />
+          </div>
+
+          <div className="form-control ">
+            <label htmlFor="fim">Fim da tarefa:</label>
+            <input type="time" name="fim"
+              onChange={(e) => setfim(e.target.value)}
+              value={fim || ""} required />
+          </div>
+          <input type="Submit" value="Criar Tarefa" />
+        </form>
+      </div>
+
+      <div className="list-todo">
+        <h2>Sua lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas !</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+
+            <h3 className={todo.done ? "todo-done" : " "}>{todo.title}</h3>
+            <p>Horário: {todo.time} as {todo.fim}   </p>
+            <div className="actions">
+              <spam onClick={() => handleEdit(todo)} >
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </spam>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+
+            </div>
+            <script>Horas()</script>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
